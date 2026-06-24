125486c7c06d753bee6cef013b2cf2f9a8f9a981
Panu Puhtila <papuht@utu.fi>
2026-06-24 18:18:20 +0300

    arxiv

--- arxivAPItest.py
+++ arxivAPItest.py
@@ -3,7 +3,9 @@ import pandas as pd
 import parquet as pq
 from io import StringIO
 
-url = 'http://export.arxiv.org/api/query?search_query=all:2026&start=0&max_results=1000'
+
+search_terms = 'all:computer-science-'+'-AND-'+'2026'
+url = f'http://export.arxiv.org/api/query?search_query={search_terms}&start=0&max_results=1000'
 data = urllib.request.urlopen(url)
 result = data.read().decode('utf-8', 'ignore')
 df = pd.read_xml(StringIO(result))
@@ -11,7 +13,7 @@ df.to_parquet("result.parquet")
 pd.read_parquet("result.parquet")
 
 
-
+print(result)
 print(data.code)
 print(pd.read_parquet)
 #print(len(feed.entries))
