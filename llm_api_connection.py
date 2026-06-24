d470e4ad31036c577c01645636cf4f1bdb2606c6
Panu Puhtila <papuht@utu.fi>
2026-06-24 15:22:49 +0300

    llm

---
+++ llm_api_connection.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+
+
+
+class LLM_connection:
+    def __init__(self, model, api_key):
+        self.model = model
+        self.api_key = api_key
+        self.url = 'https://llm.tt.utu.fi/api/generate'
+
+    def invoke(self, data, prompt_text):
+        
+        prompt = prompt_text
+        url = self.url
+
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type':'application/json'
+        }
+        payload = {
+            'model': self.model,
+            'prompt': prompt_text,
+            'stream': False,
+            'options':  {
+                "num_ctx": 65536,
+                "num_pred": 10000
+            }
+        }
+
+        response = requests.post(self.url, headers=headers, json=payload)
+        return response.json()["response"][0]
