54460ce154c9cd47a2b651d5b463ca936a802a62
Panu Puhtila <papuht@utu.fi>
2026-06-24 18:23:25 +0300

    arxiv

--- arxivAPItest.py
+++ arxivAPItest.py
@@ -4,7 +4,7 @@ import parquet as pq
 from io import StringIO
 
 
-search_terms = 'all:computer-science-'+'-AND-'+'2026'
+search_terms = 'all:computer-science-'+'-AND-'+'2026-01'
 url = f'http://export.arxiv.org/api/query?search_query={search_terms}&start=0&max_results=1000'
 data = urllib.request.urlopen(url)
 result = data.read().decode('utf-8', 'ignore')
@@ -16,15 +16,4 @@ pd.read_parquet("result.parquet")
 print(result)
 print(data.code)
 print(pd.read_parquet)
-#print(len(feed.entries))
-#for entry in feed.entries:
-#   print(entry.title)
-"""
-try:
-   with open("arxiv.txt", "w") as file:
-      file.write(result)
-except UnicodeEncodeError:
-      print("UnicodeEncodeError")
-"""
-
 
