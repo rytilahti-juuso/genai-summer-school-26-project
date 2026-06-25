960fcb9218a444f7077eefa54a8994b065f40c3e
Panu Puhtila <papuht@utu.fi>
2026-06-25 15:35:35 +0300

    llm

--- llm_connection.py
+++ llm_connection.py
@@ -1,5 +1,5 @@
 import json
-
+import re
 from llm_api_connection import LLM_connection
 from secret_key import local_key2
 from mockUpData import MOCK_ARXIV_RECORDS
@@ -7,29 +7,37 @@ from mockUpData import MOCK_ARXIV_RECORDS
 
 llama4 = "llama4:latest"
 llm = LLM_connection(llama4, local_key2)
+with open('clusterlabelingrecords.json') as json_data:
+    clusters = json.loads(json_data.read())
+    
+
+label_tag = r"<LABEL>\s*([^<]+)\s*(?:</LABEL>)?"
+cluster_tag = r"<CLUSTER>\s*([^<]+)\s*(?:</CLUSTER>)?"
+
+result_list = []
 
 
-prompt_test = "you there?"
+def get_tags(output: str, tag):
+    pattern = re.compile(tag)
+    match = pattern.search(output)
+    if match:
+        return match.group(1).strip()
+    return None
 
-prompt_text = {
+def label_clusters(llm, clusters):
+	for cluster in clusters:
+		prompt_text = {
+			"task": "Analyze the cluster and create a category label for the articles in the cluster.",
+  			"description": " First, explain your reasoning and then present the label. Don't do anything else. I want only one label for the cluster and I want it to be short and concise. I want the label inside <LABEL> tags. I want you to also ouput the cluster number inside <CLUSTER> tags.",
+  			"input": cluster,
+  			}
+		result = llm.invoke(json.dumps(prompt_text))
+		label = get_tags(result, label_tag)
+		cluster_number = get_tags(result, cluster_tag)
+		result_kv = {"cluster": cluster_number, "label": label}
+		result_list.append(result_kv)
+		print(cluster_number + " " + label)
+	return result_list
 
-  "task": "analyze",
-  "description": "Analyze the given input and provide structured view on what categories of science are present",
-  "input": MOCK_ARXIV_RECORDS,
-  "schema": {
-    "fields": [
-      {
-        "name": "string",
-        "type": "string | number | boolean | array | object",
-        "description": "Analytical dimension or metric to output."
-      }
-    ]
-  },
+label_clusters(llm, clusters)
 
-  "constraints": {
-    "output_format": "JSON",
-  }
-}
-
-result = llm.invoke(json.dumps(prompt_text))
-print(result)
