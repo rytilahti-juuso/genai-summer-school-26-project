379b83bc46b45d92a5e8e68445077600bc82f3d0
Panu Puhtila <papuht@utu.fi>
2026-06-25 15:48:12 +0300

    llm

--- llm_connection.py
+++ llm_connection.py
@@ -2,8 +2,6 @@ import json
 import re
 from llm_api_connection import LLM_connection
 from secret_key import local_key2
-from mockUpData import MOCK_ARXIV_RECORDS
-
 
 llama4 = "llama4:latest"
 llm = LLM_connection(llama4, local_key2)
